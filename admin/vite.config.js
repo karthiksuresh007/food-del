c5fc2a1dffb7fcd8a870fa9ca5759967f2d666a5
Karthik Suresh <karthiksuresh897@gmail.com>
2025-07-16 02:14:57 +0530

    Configured admin panel for Vercel

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5174 },
+  base: './',
+  server: {
+    port: 5174 
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
